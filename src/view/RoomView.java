a350e43ac86132f40283a4e0a63a7e16c95da7bb
zheng692 <zheyu.zheng@mail.utoronto.ca>
2023-11-07 14:18:20 -0500

    working on app 1

--- src/view/RoomView.java
+++ src/view/RoomView.java
@@ -1,2 +1,140 @@
-package view;public class RoomView {
+package view;
+
+
+import java.util.Collections;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.pubnub.api.PNConfiguration;
+import com.pubnub.api.PubNub;
+import com.pubnub.api.UserId;
+import com.pubnub.api.callbacks.SubscribeCallback;
+import com.pubnub.api.enums.PNStatusCategory;
+import com.pubnub.api.models.consumer.PNStatus;
+import com.pubnub.api.models.consumer.objects_api.channel.PNChannelMetadataResult;
+import com.pubnub.api.models.consumer.objects_api.uuid.PNUUIDMetadataResult;
+import com.pubnub.api.models.consumer.pubsub.PNMessageResult;
+import com.pubnub.api.models.consumer.pubsub.PNPresenceEventResult;
+import com.pubnub.api.models.consumer.pubsub.PNSignalResult;
+import com.pubnub.api.models.consumer.pubsub.files.PNFileEventResult;
+import com.pubnub.api.models.consumer.pubsub.message_actions.PNMessageActionResult;
+import com.pubnub.api.models.consumer.objects_api.membership.PNMembershipResult;
+import com.pubnub.api.PubNubException;
+import org.jetbrains.annotations.NotNull;
+
+public class RoomView {
+
+    public static void main(String[] args) throws PubNubException {
+        final UserId userId = new UserId("myUniqueUserId");
+        PNConfiguration pnConfiguration = new PNConfiguration(userId);
+        pnConfiguration.setSubscribeKey("mySubscribeKey");
+        pnConfiguration.setPublishKey("myPublishKey");
+
+        PubNub pubnub = new PubNub(pnConfiguration);
+
+        final String channelName = "myChannel";
+
+        // create message payload using Gson
+        final JsonObject messageJsonObject = new JsonObject();
+        messageJsonObject.addProperty("msg", "Hello World");
+
+        System.out.println("Message to send: " + messageJsonObject.toString());
+
+        pubnub.addListener(new SubscribeCallback() {
+
+            @Override
+            public void status(PubNub pubnub, PNStatus status) {
+                if (status.getCategory() == PNStatusCategory.PNUnexpectedDisconnectCategory) {
+                    // This event happens when radio / connectivity is lost
+                } else if (status.getCategory() == PNStatusCategory.PNConnectedCategory) {
+                    // Connect event. You can do stuff like publish, and know you'll get it.
+                    // Or just use the connected event to confirm you are subscribed for
+                    // UI / internal notifications, etc
+                    pubnub.publish()
+                            .channel(channelName)
+                            .message(messageJsonObject)
+                            .async((result, publishStatus) -> {
+                                if (!publishStatus.isError()) {
+                                    // Message successfully published to specified channel.
+                                }
+                                // Request processing failed.
+                                else {
+                                    // Handle message publish error.
+                                    // Check 'category' property to find out
+                                    // issues because of which the request failed.
+                                    // Request can be resent using: [status retry];
+                                }
+                            });
+                } else if (status.getCategory() == PNStatusCategory.PNReconnectedCategory) {
+                    // Happens as part of our regular operation. This event happens when
+                    // radio / connectivity is lost, then regained.
+                } else if (status.getCategory() == PNStatusCategory.PNDecryptionErrorCategory) {
+                    // Handle message decryption error. Probably client configured to
+                    // encrypt messages and on live data feed it received plain text.
+                }
+            }
+
+            @Override
+            public void message(PubNub pubnub, PNMessageResult message) {
+                // Handle new message stored in message.message
+                if (message.getChannel() != null) {
+                    // Message has been received on channel group stored in
+                    // message.getChannel()
+                } else {
+                    // Message has been received on channel stored in
+                    // message.getSubscription()
+                }
+
+                JsonElement receivedMessageObject = message.getMessage();
+                System.out.println("Received message: " + receivedMessageObject.toString());
+                // extract desired parts of the payload, using Gson
+                String msg = message.getMessage().getAsJsonObject().get("msg").getAsString();
+                System.out.println("The content of the message is: " + msg);
+
+                /*
+                 * Log the following items with your favorite logger - message.getMessage() -
+                 * message.getSubscription() - message.getTimetoken()
+                 */
+            }
+
+            @Override
+            public void signal(PubNub pubnub, PNSignalResult pnSignalResult) {
+
+            }
+
+            @Override
+            public void uuid(PubNub pubnub, PNUUIDMetadataResult pnUUIDMetadataResult) {
+
+            }
+
+            @Override
+            public void channel(@NotNull PubNub pubNub, @NotNull PNChannelMetadataResult pnChannelMetadataResult) {
+
+            }
+
+            @Override
+            public void membership(PubNub pubnub, PNMembershipResult pnMembershipResult) {
+
+            }
+
+            @Override
+            public void messageAction(PubNub pubnub, PNMessageActionResult pnMessageActionResult) {
+
+            }
+
+            @Override
+            public void file(@NotNull PubNub pubNub, @NotNull PNFileEventResult pnFileEventResult) {
+
+            }
+
+            @Override
+            public void presence(PubNub pubnub, PNPresenceEventResult presence) {
+
+            }
+        });
+
+        pubnub.subscribe()
+                .channels(Collections.singletonList(channelName))
+                .execute();    }
 }

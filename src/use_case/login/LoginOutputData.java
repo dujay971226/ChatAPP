e4ebe375d06dabe97751065c0f74be4190809e6e
hrdkk <junke.hou@mail.utoronto.ca>
2023-11-25 15:19:42 -0500

    version 1.7 with documentation

--- src/use_case/login/LoginOutputData.java
+++ src/use_case/login/LoginOutputData.java
@@ -3,21 +3,41 @@ package use_case.login;
 import entity.User;
 import com.pubnub.api.PubNub;
 
+/**
+ * Represents the output data produced by a login use case.
+ */
 public class LoginOutputData {
     private final User user;
     private boolean useCaseFailed;
     private PubNub config;
 
+    /**
+     * Constructs a new instance of LoginOutputData.
+     *
+     * @param user           The authenticated user.
+     * @param config         Configuration data.
+     * @param useCaseFailed  Indicates whether the use case encountered an error.
+     */
     public LoginOutputData(User user, PubNub config, boolean useCaseFailed) {
         this.user = user;
         this.useCaseFailed = useCaseFailed;
         this.config = config;
     }
 
+    /**
+     * Gets the authenticated user.
+     *
+     * @return The authenticated user.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Gets the configuration data.
+     *
+     * @return Configuration data.
+     */
     public PubNub getConfig() {
         return config;
     }

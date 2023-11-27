9c78d6f28f5320953d350d7e8ac98f85e50240d9
lixiaof8 <xiaof.li@mail.utoronto.ca>
2023-11-27 15:20:40 -0500

    fixed login view

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -4,6 +4,7 @@ import com.pubnub.api.PubNubException;
 import interface_adapter.login.LoginController;
 import interface_adapter.login.LoginState;
 import interface_adapter.login.LoginViewModel;
+import interface_adapter.signup.SignupState;
 
 import javax.swing.*;
 import java.awt.*;
@@ -134,6 +135,12 @@ public class LoginView extends JPanel implements ActionListener, PropertyChangeL
     public void propertyChange(PropertyChangeEvent evt) {
         LoginState state = (LoginState) evt.getNewValue();
         setFields(state);
+        if(evt.getSource().equals(loginViewModel)){
+
+            if (state.getUsernameError() != null) {
+                JOptionPane.showMessageDialog(this, state.getUsernameError());
+            }
+        }
     }
 
     private void setFields(LoginState state) {
